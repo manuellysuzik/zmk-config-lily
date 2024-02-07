b6dc418a5bcbef51a6c61694513d12424337a673
Manuelly Suzik <manuellysuzik@gmail.com>
2024-02-07 00:37:56 -0300

    Update conf.h

--- config/conf.h
+++ config/conf.h
@@ -1,9 +1,9 @@
-#define ENCODERS_PAD_A \
+#define ENCODERS_PAD_A_RIGHT \
     {                  \
         F4             \
     } // Pro Micro pin on Out A
-#define ENCODERS_PAD_B \
+#define ENCODERS_PAD_B_RIGHT \
     {                  \
         D4             \
     }                         // Pro Micro pin on Out B
-#define ENCODER_RESOLUTIONS 4 // Pulses the encoder registers between each detent
+#define ENCODER_RESOLUTIONS_RIGHT 4 // Pulses the encoder registers between each detent
