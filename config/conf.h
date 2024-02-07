f9b9bcce6422dccc836d2745104f1d12a3dde032
Manuelly Suzik <manuellysuzik@gmail.com>
2024-02-07 00:52:37 -0300

    change pins

--- config/conf.h
+++ config/conf.h
@@ -1,9 +1,9 @@
 #define ENCODERS_PAD_A_RIGHT \
-    {                  \
-        F4             \
+    {                        \
+        D21                  \
     } // Pro Micro pin on Out A
 #define ENCODERS_PAD_B_RIGHT \
-    {                  \
-        D4             \
-    }                         // Pro Micro pin on Out B
+    {                        \
+        D4                   \
+    }                               // Pro Micro pin on Out B
 #define ENCODER_RESOLUTIONS_RIGHT 4 // Pulses the encoder registers between each detent
